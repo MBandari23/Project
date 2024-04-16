0a819aa1b0ebac7667f2a97d074fe9d7b34f48a7
MBandari23 <143135532+MBandari23@users.noreply.github.com>
2024-04-15 20:26:26 -0400

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import numpy as np
+import pandas as pd
 from PIL import Image
 import os
+import pickle
+import requests
+from io import BytesIO
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import boto3
 
+
+
+
+
 app = Flask(__name__)
+
+app.config.from_object('config.DevConfig')
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return ('.' in filename) and (filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 #initializing boto3 client
 s3_client = boto3.client('s3', region_name='us-east-2')
@@ -28,39 +45,24 @@ def home():
 def mybirdwat_page():
     return render_template('mybirdwat.html')
 
-@app.route('/predict', methods=['POST']) #upload and prediction route
+@app.route('/predict', methods=['GET', 'POST']) #upload and prediction route
 def predict():
-    #if 'bird_image' not in request.files:
-        #flash('file part is missing')
-        #return redirect(request.url) #pages loads back to MyBirdWat page
-    file = request.files['bird_image'] #gets the file from the key "bird_image"
-    if file.filename== '':
-        flash('No file is selected') #notifies the user that no file is selected
+    if request.method == 'POST':
+        file = request.files['bird_image'] #gets the file from the key "bird_image"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['uploads'], filename)
+            file.save(filepath) 
+            print("saved")
+            
 
-        return redirect(request.url) 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath) 
-        print("saved")
 
-        #resizing and preprocessing the image file for the prediction
-        imge = Image.open(filepath) #this opens the image file
-        imge= imge.resize((229,225)) #Resizing the image as required
-        imge.show()
-        print("done")
-        imge_array = np.expand_dims(np.array(imge) / 255.0, axis=0)
-        print(f"Resized image size: {imge.size}")
 
-        #predictions = model.predict(img_array)
-        #pred_species = np.argmax(predictions)
-
-        s3_client.upload_file(filepath, 'birdbucket23', 'uploads/' + filename)
-
-        #diplay the image results
         return render_template('prediction_result.html') 
 
-
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('error.html'), 500
 
 @app.route('/about')
 def about_page():
